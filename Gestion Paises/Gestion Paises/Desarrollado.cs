e422b2153c592e61b0196359ac6b36fd13faac46
Francisco <francisco.anticona81@gmail.com>
2024-04-14 23:57:15 -0500

    Creando Clase De Desarrollado - Francisco Anticona
    Dios nos ampare x2, me equivoque y subi el otro en main jejeje

---
+++ Gestion Paises/Gestion Paises/Desarrollado.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Paises
+{
+    internal class Desarrollado : Pais
+    {
+        public Desarrollado(string name, float poblacion, float dinero, float salud, float educacion, float seguridad, float felicidad, float gastos, float impuestos)
+        {
+            this.NombreNivel = name;
+            this.poblacion = poblacion;
+            this.dinero = dinero;
+            this.salud = salud;
+            this.educacion = educacion;
+            this.seguridad = seguridad;
+            this.felicidad = felicidad;
+            this.gastos = gastos;
+            this.impuestos = impuestos;
+        }
+
+        public override void Healt(float a)
+        {
+            if (a != 0)
+            {
+                salud = a;
+            }
+            if (salud < 80)
+            {
+                poblacion--;
+            }
+            else if (salud >= 80)
+            {
+                poblacion++;
+            }
+        }
+        public override void Education(float a)
+        {
+            if (a != 0)
+            {
+                educacion = a;
+            }
+            dinero = dinero - educacion;
+        }
+        public override void Security(float a)
+        {
+            if (a != 0)
+            {
+                seguridad = a;
+            }
+            if (seguridad < 80)
+            {
+                felicidad--;
+            }
+            else if (salud >= 80)
+            {
+                felicidad++;
+            }
+        }
+
+        public override void CalculateFactor()
+        {
+            gastos = salud + educacion + seguridad;
+            dinero = (dinero + impuestos) - gastos;
+        }
+    }
+}
