c336adcd588432fbf08bf482744e60edddce21e2
AdelaRomeroV <ajrvjarinee17@gmail.com>
2024-04-14 21:01:59 -0500

    Plantilla Pais - Adela Romero

---
+++ Gestion Paises/Gestion Paises/Pais.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Paises
+{
+    internal class Pais
+    {
+        float Impuestos;
+        float Poblacion;
+        float Dinero;
+        float Salud;
+        float Educacion;
+        float Seguridad;
+        float Felicidad;
+        float Gastos;
+
+        string NombreNivel;
+
+        public void CalculateFactor()
+        {
+
+        }
+
+        public void Healt()
+        {
+
+        }
+
+        public void Education()
+        {
+
+        }
+
+        public void Security()
+        {
+
+        }
+
+    }
+}
