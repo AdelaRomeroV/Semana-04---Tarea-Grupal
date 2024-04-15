65a86d549dc15e3a688afb2ab09f9e5428acd91e
AndreiMore <andreiloayza14@gmail.com>
2024-04-15 12:48:46 -0500

    Void SelectDifficult - Jose Andree Loayza More
    Logica del void SelectDifficult

---
+++ Gestion Paises/Gestion Paises/Game.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_Paises
+{
+    internal class Game
+    {
+        public void SelectDifficul()
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Selecciona el nivel de desarrollo de tu país");
+                Console.WriteLine("1. Subdesarrolado");
+                Console.WriteLine("2. En Desarrollo");
+                Console.WriteLine("3. Desarrollado\n");
+
+                Console.ForegroundColor = ConsoleColor.White;
+                int opt = int.Parse(Console.ReadLine());
+
+                switch (opt)
+                {
+                    case 1:
+                        {
+                            GetSubdesarrollado();
+                            flag = false;
+                            break;
+                        }
+                    case 2:
+                        {
+                            GetSubdesarrollado();
+                            flag = false;
+                            break;
+                        }
+                    case 3:
+                        {
+                            GetDesarrollado();
+                            flag = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Opcion Invalida");
+                            break;
+                        }
+                }
+            }
+        }
+
+        void MenuGame()
+        {
+
+        }
+        
+        void ViewStats()
+        {
+
+        }
+
+        void PassTurn()
+        {
+
+        }
+
+        void ChangeTaxes()
+        {
+
+        }
+
+        void Reset()
+        {
+
+        }
+        
+        void GetSubdesarrollado()
+        {
+
+        }
+
+        void GetDesarrollo()
+        {
+
+        }
+
+        void GetDesarrollado()
+        {
+
+        }
+
+        void FinDelJuego()
+        {
+
+        }
+    }
+}
